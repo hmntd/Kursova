b70b2122b1219f1c1ebe5df78409d8d3adf0df49
hmntd <bazavluk.r.s@gmail.com>
2024-12-02 03:31:36 +0200

    Filtering, Food

---
+++ Models/Suply.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReestrForm.Models
+{
+    public class Suply
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Type { get; set; }
+        public string Path_to_Image { get; set; }
+        public int WasBought { get; set; }
+        public Suply () { }
+        public Suply(string id, string name, decimal price, int wasBought)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            WasBought = wasBought;
+        }
+    }
+}
